b00e8160b40b2310862bef7430495347a2229bae
Tanim Sk <sktanim5800@gmail.com>
2024-02-14 16:53:10 +0600

    banner

--- app/Http/Controllers/Admin/SellerLivesController.php
+++ app/Http/Controllers/Admin/SellerLivesController.php
@@ -11,92 +11,94 @@ use Illuminate\Support\Facades\Response;
 
 class SellerLivesController extends Controller
 {
-    function Index(){
-        $data=Live::all();
+    function Index()
+    {
+        $data = Live::all();
 
 
-        $new_data=array();
-        foreach ($data as $value){
+        $new_data = array();
+        foreach ($data as $value) {
 
 
-            $seller_info=Seller::where("id",$value->seller_id)->first();
-            $seller_data=array();
+            $seller_info = Seller::where("id", $value->seller_id)->first();
+            $seller_data = array();
 
-            array_push($seller_data,[
-                "id"=> $seller_info->id,
-                "shop_name"=> $seller_info->shop_name,
-                "phone"=>$seller_info->phone,
-                "business_type_name"=> BusinessType::where("id",$seller_info->business_type_id)->first()->name,
-                "address"=>$seller_info->address,
-                "slug"=>$seller_info->slug,
-                "email"=>$seller_info->email,
-                "logo"=>config("app.url")."storage/logo/".$seller_info->logo
+            array_push($seller_data, [
+                "id" => $seller_info->id,
+                "shop_name" => $seller_info->shop_name,
+                "phone" => $seller_info->phone,
+                "business_type_name" => BusinessType::where("id", $seller_info->business_type_id)->first() != null ? BusinessType::where("id", $seller_info->business_type_id)->first()->name : null,
+                "address" => $seller_info->address,
+                "slug" => $seller_info->slug,
+                "email" => $seller_info->email,
+                "logo" => config("app.url") . "storage/logo/" . $seller_info->logo
             ]);
 
-            array_push($new_data,[
-                "seller_info"=>$seller_data,
-                "live_info"=>[
-                    "id"=>$value->id,
-                    "seller_id"=>$value->seller_id,
-                    "fb_rtmp"=>$value->fb_rtmp,
-                    "youtube_rtmp"=>$value->youtube_rtmp,
-                    "products"=>json_decode($value->products),
-                    "is_ended"=>$value->is_ended,
-                    "created_at"=>$value->created_at,
-                    "updated_at"=>$value->updated_at,
+            array_push($new_data, [
+                "seller_info" => $seller_data,
+                "live_info" => [
+                    "id" => $value->id,
+                    "seller_id" => $value->seller_id,
+                    "fb_rtmp" => $value->fb_rtmp,
+                    "youtube_rtmp" => $value->youtube_rtmp,
+                    "products" => json_decode($value->products),
+                    "is_ended" => $value->is_ended,
+                    "created_at" => $value->created_at,
+                    "updated_at" => $value->updated_at,
                 ]
             ]);
         }
 
 
         return Response::json([
-            "error"=>false,
-            "data"=>$new_data
+            "error" => false,
+            "data" => $new_data
         ]);
 
     }
 
-    function SellerTotalLives($id){
-        $data=Live::where("seller_id",$id)->get();
+    function SellerTotalLives($id)
+    {
+        $data = Live::where("seller_id", $id)->get();
 
 
-        $new_data=array();
-        foreach ($data as $value){
+        $new_data = array();
+        foreach ($data as $value) {
 
 
-            $seller_info=Seller::where("id",$value->seller_id)->first();
-            $seller_data=array();
+            $seller_info = Seller::where("id", $value->seller_id)->first();
+            $seller_data = array();
 
-            array_push($seller_data,[
-                "id"=> $seller_info->id,
-                "shop_name"=> $seller_info->shop_name,
-                "phone"=>$seller_info->phone,
-                "business_type_name"=> BusinessType::where("id",$seller_info->business_type_id)->first()->name,
-                "address"=>$seller_info->address,
-                "slug"=>$seller_info->slug,
-                "email"=>$seller_info->email,
-                "logo"=>config("app.url")."storage/logo/".$seller_info->logo
+            array_push($seller_data, [
+                "id" => $seller_info->id,
+                "shop_name" => $seller_info->shop_name,
+                "phone" => $seller_info->phone,
+                "business_type_name" => BusinessType::where("id", $seller_info->business_type_id)->first()->name,
+                "address" => $seller_info->address,
+                "slug" => $seller_info->slug,
+                "email" => $seller_info->email,
+                "logo" => config("app.url") . "storage/logo/" . $seller_info->logo
             ]);
 
-            array_push($new_data,[
-                "seller_info"=>$seller_data,
-                "live_info"=>[
-                    "id"=>$value->id,
-                    "seller_id"=>$value->seller_id,
-                    "fb_rtmp"=>$value->fb_rtmp,
-                    "youtube_rtmp"=>$value->youtube_rtmp,
-                    "products"=>json_decode($value->products),
-                    "is_ended"=>$value->is_ended,
-                    "created_at"=>$value->created_at,
-                    "updated_at"=>$value->updated_at,
+            array_push($new_data, [
+                "seller_info" => $seller_data,
+                "live_info" => [
+                    "id" => $value->id,
+                    "seller_id" => $value->seller_id,
+                    "fb_rtmp" => $value->fb_rtmp,
+                    "youtube_rtmp" => $value->youtube_rtmp,
+                    "products" => json_decode($value->products),
+                    "is_ended" => $value->is_ended,
+                    "created_at" => $value->created_at,
+                    "updated_at" => $value->updated_at,
                 ]
             ]);
         }
 
 
         return Response::json([
-            "error"=>false,
-            "data"=>$new_data
+            "error" => false,
+            "data" => $new_data
         ]);
 
     }
